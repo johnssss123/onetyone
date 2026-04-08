8cb5902ee8f8f1d1a11072d649d736e77c09cec9
johnssss123 <jr40201466@gmail.com>
2026-04-09 00:00:54 +0545

    registration, login and JWTauth complete

---
+++ src/main/java/com/onetyone/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.onetyone.controller;
+
+import com.onetyone.dto.AuthResponse;
+import com.onetyone.dto.LoginRequest;
+import com.onetyone.dto.RegisterRequest;
+import com.onetyone.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
